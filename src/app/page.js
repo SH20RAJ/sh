172a2cec29dfb3058edfb95ce73a7ed337915af4
sh20raj <shaswatraj3@gmail.com>
2024-04-20 22:09:52 +0530

    ui + mode

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
+'use client'
+import { Main } from "@/components/main";
 import Image from "next/image";
+import { useState } from "react";
+
 
 export default function Home() {
+
   return (
     <>
-    
+    <Main/>
     </>
   );
 }
